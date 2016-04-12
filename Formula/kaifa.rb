39aaaa0070242773e2904ccf699d267cafa2a48c
Jun Lin <linjunpop@gmail.com>
2016-04-12 21:50:01 +0800

    Bump to v0.1.0.

--- Formula/kaifa.rb
+++ Formula/kaifa.rb
@@ -1,8 +1,8 @@
 class Kaifa < Formula
   desc "Command-line tool for kaifa.li"
   homepage "https://github.com/kaifali/cli"
-  url "https://github.com/kaifali/cli/archive/v0.0.3.tar.gz"
-  sha256 "5ae3502171c330cb6b778fa30af96dcf30e30beb297ea52f4e052038821ced02"
+  url "https://github.com/kaifali/cli/archive/v0.1.0.tar.gz"
+  sha256 "b5f248e830f0ed8f3379fb4fc8d486b824186ce4460d36000bc5d551f10e6d1c"
 
   head "https://github.com/kaifali/cli.git"
 
